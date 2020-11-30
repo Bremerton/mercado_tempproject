ace33566a7c83f3d11fc06aca3918d533496abfd
Bremerton <72746036+Bremerton@users.noreply.github.com>
2020-11-30 07:25:20 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+input.set_loud_sound_threshold(200)
 def on_loud_sound():
     pass
-    music.ba_ding.play()
     light.show_animation(light.rainbowAnimation, 10000)
-    music.ba_ding.stop()
 input.on_loud_sound(on_loud_sound)
+pause(10000)
+light.stop_all_animations()
+light.clear()
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
+input.setLoudSoundThreshold(200)
 input.onLoudSound(function on_loud_sound() {
     
-    music.baDing.play()
     light.showAnimation(light.rainbowAnimation, 10000)
-    music.baDing.stop()
 })
+pause(10000)
+light.stopAllAnimations()
+light.clear()
