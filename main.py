542cdf807c5d5396babdb77479660dc35f6dc51f
Bremerton <72746036+Bremerton@users.noreply.github.com>
2020-11-30 16:15:25 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-light.clear()
-input.set_loud_sound_threshold(200)
+input.set_loud_sound_threshold(230)
 def on_loud_sound():
     pass
-    light.show_animation(light.running_lights_animation, 10000)
-    pause(10000)
+    light.show_animation(light.comet_animation, 10000)
     light.clear()
 input.on_loud_sound(on_loud_sound)
--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
-light.clear()
-input.setLoudSoundThreshold(200)
+input.setLoudSoundThreshold(230)
 input.onLoudSound(function on_loud_sound() {
     
-    light.showAnimation(light.runningLightsAnimation, 10000)
-    pause(10000)
+    light.showAnimation(light.cometAnimation, 10000)
     light.clear()
 })
